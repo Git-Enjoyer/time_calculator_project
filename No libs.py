dfd372517e43248634a9045f7d5a4460aa29b9a2
git-enjoyer <jakelogan35@gmail.com>
2024-08-13 09:30:11 +0100

    Final version with no imported libraries

--- No libs.py
+++ No libs.py
@@ -6,8 +6,8 @@ def add_time(start, duration, start_day=''):
     end_minute = start_minute + duration_minutes
     additional_hours = end_minute // 60
     end_minute %= 60
-    end_hour = start_hour + duration_hours + additional_hours
 
+    end_hour = start_hour + duration_hours + additional_hours
     if start_period == 'PM':
         end_hour += 12
 
@@ -15,9 +15,10 @@ def add_time(start, duration, start_day=''):
     end_hour %= 24
 
     end_period = 'AM' if end_hour < 12 else 'PM'
-    end_hour = end_hour if end_hour <= 12 else end_hour - 12
     if end_hour == 0:
         end_hour = 12
+    elif end_hour > 12:
+        end_hour -= 12
 
     formatted_time = f'{end_hour}:{end_minute:02} {end_period}'
 
@@ -29,15 +30,20 @@ def add_time(start, duration, start_day=''):
         new_weekday_index = (current_weekday + additional_days) % 7
         new_day_of_week = day_of_week[new_weekday_index]
 
-        if additional_days == 1:
-            days_later = '(next day)'
-        elif additional_days > 1:
-            days_later = f'({additional_days} days later)'
+    if additional_days == 1:
+        days_later = '(next day)'
+    elif additional_days > 1:
+        days_later = f'({additional_days} days later)'
 
     if new_day_of_week:
-        print(f"{formatted_time}, {new_day_of_week} {days_later}")
+        result = f"{formatted_time}, {new_day_of_week}"
+        if days_later:
+            result += f" {days_later}"
+        return result
     else:
-        print(f"{formatted_time} {days_later}")
+        result = formatted_time
+        if days_later:
+            result += f" {days_later}"
+        return result
 
 
-add_time('8:16 PM', '466:02', 'tuesday')
